33f1e56fa64a1f6648283b9e52736a7147a331b5
George Chokhonelidze <g_chokhonelidze@outlook.com>
2022-04-01 17:17:17 -0500

    fix"

--- tic-tac-toe/src/parts/squere.js
+++ tic-tac-toe/src/parts/squere.js
@@ -1,5 +1,8 @@
 const Squere = ({ id, board, setBoard, player, setPlayer }) => {
     const playerName = ["", "X", "O"];
+    const colors = ['#27e511','#d7e511','#e51111d4','#198754'];
+    let random = Math.floor(Math.random()*colors.length);
+    let color = colors[random];
     return (
       <button
         onClick={() => {
@@ -12,7 +15,7 @@ const Squere = ({ id, board, setBoard, player, setPlayer }) => {
           }
         }}
       >
-        <h1>{playerName[board[id]]}</h1>
+        <h1 style={{color:color}}>{playerName[board[id]]}</h1>
       </button>
     );
   };
